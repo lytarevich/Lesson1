727395c810502559f7397743e947fc15bbe8bcba
lytarevich <124921376+lytarevich@users.noreply.github.com>
2023-03-02 14:36:40 +0300

    Add files via upload
    1ая задача

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-int a = 2;
-int b = 3;
-int c = 7;
-int max = a;
-if (a>max) max = a;
-if (b>max) max = b;
-if (c>max) max = c;
-Console.WriteLine(max);
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+int a = 5;
+int b = 7;
+if (a>b)
+{
+    Console.WriteLine (a);
+}
+else
+{
+    Console.WriteLine (b);
+}
