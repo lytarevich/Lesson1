7fb6e40f3474b31339b697cf57a9e1b45c65e7e8
lytarevich <124921376+lytarevich@users.noreply.github.com>
2023-03-02 14:38:08 +0300

    Add files via upload
    2ая задача

---
+++ Program2.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a = 2;
+int b = 3;
+int c = 7;
+int max = a;
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+Console.WriteLine(max);
